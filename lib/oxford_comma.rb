3b8cfb9bc62b2d0c14f2204e0576ad5ffb3d21f7
Raul Figueroa <raul.a.figueroa@hotmail.com>
2019-08-16 11:01:26 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,7 +1,11 @@
 def oxford_comma(array)
-  array.join (",")
-  array.join (" and ") 
-  if  array > 3 
-    
+  if array.size == 1
+    return array[0]
+  elsif array.size == 2
+    return array.join(" and ")
+  else
+    return array[0..-2].join(', ') + ", and " + array[-1]
+  end
 end
+
 
